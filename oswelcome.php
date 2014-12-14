aee6707e97fe8c31d0da9b5f959ff5afcba95b9d
chrisx84 <chrisx84@live.ca>
2014-12-14 11:30:22 -0500

    welcome page header updated
    sorry forgot to add the required header info for bootstrap to fully
    work.

--- oswelcome.php
+++ oswelcome.php
@@ -189,9 +189,13 @@ if (is_dir($dir))
 	$jquerypics = str_replace(", ~~", "", $jquerypics);
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 <head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
   <title>Welcome to <?php echo $grid_name; ?></title>
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
   <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
